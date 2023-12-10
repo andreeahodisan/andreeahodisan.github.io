66bf8a8670a80d405bd992e5db673414e08d0ba9
Andreea Hodisan <andreeasabau14@yahoo.com>
2023-12-10 12:01:38 -0800

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,17 @@
+console.info(2 + 3);
+console.warn("Hodisan Andreea");
+
+var employed = true;
+var age = 26;
+console.log("Angajat", employed);
+employed = false;
+console.log("Angajat", employed);
+employed = "da";
+console.log("Angajat", employed);
+
+var skills = ["html", "css"];
+console.debug("tipul variabilei skills", typeof skills);
+
+var job = document.getElementById("job-titles");
+console.warn("job", job);
+console.info(typeof job);
