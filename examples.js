0c5864fadb638fbf27a67c745e2a877033cb7abc
Andreea Hodisan <andreeasabau14@yahoo.com>
2023-12-10 12:18:07 -0800

    add function examples

--- examples.js
+++ examples.js
@@ -1,17 +1,21 @@
-console.info(2 + 3);
-console.warn("Hodisan Andreea");
+function variableExamples() {
+  console.info(2 + 3);
+  console.warn("Hodisan Andreea");
 
-var employed = true;
-var age = 26;
-console.log("Angajat", employed);
-employed = false;
-console.log("Angajat", employed);
-employed = "da";
-console.log("Angajat", employed);
+  var employed = true;
+  var age = 26;
+  console.log("Angajat", employed);
+  employed = false;
+  console.log("Angajat", employed);
+  employed = "da";
+  console.log("Angajat", employed);
 
-var skills = ["html", "css"];
-console.debug("tipul variabilei skills", typeof skills);
+  var skills = ["html", "css"];
+  console.debug("tipul variabilei skills", typeof skills);
 
-var job = document.getElementById("job-titles");
-console.warn("job", job);
-console.info(typeof job);
+  var job = document.getElementById("job-titles");
+  console.warn("job", job);
+  console.info(typeof job);
+}
+
+variableExamples();
