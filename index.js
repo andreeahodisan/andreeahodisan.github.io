1b659c11487db3a630af6c2a49c1a647a7e40eed
Andreea Hodisan <andreeasabau14@yahoo.com>
2023-12-10 16:36:18 -0800

    add show function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.info("page", page);
+  page.style.display = "block";
+}
+
 function hide(id) {
   console.info("hide", id);
   document.getElementById(id).style.display = "none";
@@ -7,34 +14,26 @@ function showHome() {
   hide("projects");
   hide("languages");
   hide("skills");
-
-  var page = document.getElementById("home");
-  page.style.display = "block";
+  show("home");
 }
 
 function showSkills() {
   hide("projects");
   hide("languages");
   hide("home");
-
-  var page = document.getElementById("skills");
-  page.style.display = "block";
+  show("skills");
 }
 
 function showProjects() {
   hide("skills");
   hide("languages");
   hide("home");
-
-  var page = document.getElementById("projects");
-  page.style.display = "block";
+  show("projects");
 }
 
 function showLanguages() {
   hide("projects");
   hide("home");
   hide("skills");
-
-  var page = document.getElementById("languages");
-  page.style.display = "block";
+  show("languages");
 }
